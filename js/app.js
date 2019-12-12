1a4ad43a00e6241e959550a4fae03af9694c5dcf
Joshua Chan <JChan@ucvts.tec.nj.us>
2019-12-12 12:49:11 -0500

    css is old code

---
+++ js/app.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.getElementById("add-to-list").onclick = addList;
+    document.getElementById("mark-as-complete").onclick = markComplete;
+    document.getElementById("priority-of-item").onclick = itemPriority;
+    document.getElementById("delete-item").onclick = deleteItem;

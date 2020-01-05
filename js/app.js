23561a896998f085ef64aa04143196284d98ce04
JoshuaChan308 <55455749+JoshuaChan308@users.noreply.github.com>
2020-01-05 13:19:10 -0500

    Variables

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 let elements = []
+let prioritize_elements;
+let priority_button = document.getElementsByClassName("button_priority");
+let complete_button = document.getElementsByClassName("complete_button");
+let remove_button = document.getElementsByClassName("remove_button");
+let list_item = document.getElementsByClassName("to_do")
+var add = document.getElementById("add_button");
+var row = document.getElementsByClassName("row");

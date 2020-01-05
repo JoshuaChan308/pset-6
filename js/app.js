e9400ef27013ba657da041d94e054849669ea2ca
JoshuaChan308 <55455749+JoshuaChan308@users.noreply.github.com>
2020-01-05 11:50:25 -0500

    cleared js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,1 @@
-window.onload = function() {
-    document.getElementById("add-to-list").onclick = addList;
-    document.getElementById("mark-as-complete").onclick = markComplete;
-    document.getElementById("priority-of-item").onclick = itemPriority;
-    document.getElementById("delete-item").onclick = deleteItem;
-}
 
-const addList = function() {
-const item = [];
-let canvas = document.getElementById("student-canvas-1");
-let ctx = canvas.getContext("2d");
-
-};
